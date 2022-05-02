14b50eb8ea8d525db820a6f1e0498b6510249309
ellenmariadev <ellenmariadev@gmail.com>
2022-05-02 19:54:26 -0300

    media queries

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-
 function redirecionar(pag) {
-    location.replace(pag);
+  location.replace(pag);
 }
 
-function  promptEscolha (pergunta, primeiraPagina, segundaPagina) {
-    let entrada; 
+function promptEscolha(pergunta, primeiraPagina, segundaPagina) {
+  let entrada;
 
-    while (entrada !== 1 && entrada !== 2) {
-      entrada = Number(prompt(pergunta));
-    } if (entrada === 1) {
-      redirecionar(primeiraPagina);
-    } else if (entrada === 2) {
-      redirecionar(segundaPagina);
+  while (entrada !== 1 && entrada !== 2) {
+    entrada = Number(prompt(pergunta));
+  }
+  if (entrada === 1) {
+    redirecionar(primeiraPagina);
+  } else if (entrada === 2) {
+    redirecionar(segundaPagina);
   }
 }
